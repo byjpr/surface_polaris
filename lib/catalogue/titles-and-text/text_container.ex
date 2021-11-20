dfc4459bf2a54c766209b0ba828b64388ab978b7
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-20 01:33:26 +0000

    Add catalogues for text container and text style

---
+++ lib/catalogue/titles-and-text/text_container.ex
@@ -0,0 +1,24 @@
+defmodule SurfacePolaris.Catalogue.TextContainer do
+  @moduledoc """
+  """
+  use Surface.Catalogue.Example,
+    subject: SurfacePolaris.TitlesText.TextContainer,
+    catalogue: SurfacePolaris.Catalogue,
+    title: "Extra large",
+    height: "120px",
+    container: {:div, class: "temporary-workaround"}
+
+  alias SurfacePolaris.TitlesText.Heading
+
+  def render(assigns) do
+    ~F"""
+    <TextContainer>
+      <Heading>Install the Shopify POS App</Heading>
+      <p>
+      Shopify POS is the easiest way to sell your products in person. Available
+      for iPad, iPhone, and Android.
+      </p>
+    </TextContainer>
+    """
+  end
+end
---
+++ lib/catalogue/titles-and-text/text_style.ex
@@ -0,0 +1,16 @@
+defmodule SurfacePolaris.Catalogue.TextStyle do
+  @moduledoc """
+  """
+  use Surface.Catalogue.Example,
+    subject: SurfacePolaris.TitlesText.TextStyle,
+    catalogue: SurfacePolaris.Catalogue,
+    title: "Extra large",
+    height: "120px",
+    container: {:div, class: "temporary-workaround"}
+
+  def render(assigns) do
+    ~F"""
+    <TextStyle variation="subdued">No supplier listed</TextStyle>
+    """
+  end
+end
