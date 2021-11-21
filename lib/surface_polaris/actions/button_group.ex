cd675ff6cef0fcdd3eff3cdc3826b349a941b5de
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-21 16:45:51 +0000

    add buttongroup

---
+++ lib/surface_polaris/actions/button_group.ex
@@ -0,0 +1,26 @@
+defmodule SurfacePolaris.Actions.ButtonGroup do
+  @moduledoc false
+
+  use Surface.Component
+  prop(connectedTop, :boolean, default: false)
+  prop(fullWidth, :boolean, default: false)
+  prop(segmented, :boolean, default: false)
+  prop(spacing, :string, values: ~w(default extraTight tight loose), default: "default")
+
+  slot(default)
+
+  def render(assigns) do
+    ~F"""
+    <div class={[
+      "Polaris-ButtonGroup",
+      "Polaris-ButtonGroup--fullWidth": @fullWidth,
+      "Polaris-ButtonGroup--segmented": @segmented,
+      "Polaris-ButtonGroup--extraTight": @spacing == "extraTight",
+      "Polaris-ButtonGroup--tight": @spacing == "tight",
+      "Polaris-ButtonGroup--loose": @spacing == "loose",
+    ]}>
+      <#slot />
+    </div>
+    """
+  end
+end
