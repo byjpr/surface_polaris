d70b53d77cc7078e71180dd414822f52c22799c0
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-21 16:47:21 +0000

    add second catalogue example

--- lib/catalogue/feedback-indicators/banner.ex
+++ lib/catalogue/feedback-indicators/banner.ex
@@ -1,18 +1,45 @@
 defmodule SurfacePolaris.Catalogue.Banner do
   @moduledoc """
   """
-  use Surface.Catalogue.Example,
-    subject: SurfacePolaris.Feedback.Banner,
-    catalogue: SurfacePolaris.Catalogue,
-    title: "Default banners",
-    height: "150px",
-    container: {:div, class: "temporary-workaround"}
+  defmodule BasicBanner do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Feedback.Banner,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Default banners",
+      height: "150px",
+      container: {:div, class: "temporary-workaround"}
 
-  def render(assigns) do
-    ~F"""
-      <Banner title="Order archived">
-        <p>This order was archived on March 7, 2017 at 3:12pm EDT.</p>
-      </Banner>
-    """
+    def render(assigns) do
+      ~F"""
+        <Banner title="Order archived">
+          <p>This order was archived on March 7, 2017 at 3:12pm EDT.</p>
+        </Banner>
+      """
+    end
+  end
+
+  defmodule BannerFooterCTA do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Feedback.Banner,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Banner with footer call-to-action",
+      height: "250px",
+      container: {:div, class: "temporary-workaround"}
+
+    def render(assigns) do
+      ~F"""
+        <Banner
+          title="Some of your product variants are missing weights"
+          status="warning"
+          action={[content: "Edit variant weights", url: "http://localhost:4000/components/SurfacePolaris.Feedback.Banner/example"]}
+          secondaryAction={[content: "Edit variant weights", url: "http://localhost:4000/components/SurfacePolaris.Feedback.Banner/example"]}
+        >
+          <p>
+          Add weights to show accurate rates at checkout and when buying shipping
+          labels in Shopify.
+          </p>
+        </Banner>
+      """
+    end
   end
 end
