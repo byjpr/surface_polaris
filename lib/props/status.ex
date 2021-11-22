a502de94bb119cf95e5a00d7c60fc1eec2db5411
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-22 19:33:09 +0000

    add status

---
+++ lib/props/status.ex
@@ -0,0 +1,20 @@
+defmodule SurfacePolaris.StatusProp do
+  @moduledoc false
+  defmacro __using__(component_doc_name) do
+    component_doc_name =
+      if component_doc_name != [] do
+        component_doc_name
+      else
+        Module.split(__CALLER__.module) |> List.last() |> String.downcase()
+      end
+
+    quote do
+      @doc "The status of #{unquote(component_doc_name)}."
+
+      prop(status, :string,
+        default: "default",
+        values: ~w(default success info attention critical warning new)
+      )
+    end
+  end
+end
