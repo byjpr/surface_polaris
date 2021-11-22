f3e18bf6e3a7139458b35dc3407ed4e0fecc90c7
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-22 20:16:15 +0000

    add layout catalogue

--- lib/catalogue/structure/layout.ex
+++ lib/catalogue/structure/layout.ex
@@ -2,13 +2,14 @@ defmodule SurfacePolaris.Catalogue.Layout do
   @moduledoc """
   """
 
-  defmodule H1 do
+  defmodule OneColumn do
     use Surface.Catalogue.Example,
       subject: SurfacePolaris.Structure.Layout,
       catalogue: SurfacePolaris.Catalogue,
-      title: "h1",
+      title: "One-column layout",
       height: "190px",
-      container: {:div, class: "temporary-workaround"}
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
 
     alias SurfacePolaris.Structure.Card
 
@@ -25,13 +26,14 @@ defmodule SurfacePolaris.Catalogue.Layout do
     end
   end
 
-  defmodule H2 do
+  defmodule TwoPrimarySecondary do
     use Surface.Catalogue.Example,
       subject: SurfacePolaris.Structure.Layout,
       catalogue: SurfacePolaris.Catalogue,
-      title: "h2",
-      height: "290px",
-      container: {:div, class: "temporary-workaround"}
+      title: "Two columns with primary and secondary widths",
+      height: "190px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
 
     alias SurfacePolaris.Structure.Card
 
@@ -46,6 +48,96 @@ defmodule SurfacePolaris.Catalogue.Layout do
         <Layout.Section secondary>
           <Card title="Tags" sectioned>
             <p>Add tags to your order.</p>
+          </Card>
+        </Layout.Section>
+      </Layout>
+      """
+    end
+  end
+
+  defmodule TwoEqual do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Structure.Layout,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Two columns with equal width",
+      height: "270px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
+
+    alias SurfacePolaris.Structure.Card
+    alias SurfacePolaris.TitlesText.TextStyle
+
+    def render(assigns) do
+      ~F"""
+      <Layout>
+        <Layout.Section oneHalf>
+          <Card title="Florida">
+            <Card.Section>
+              <TextStyle variation="subdued">455 units available</TextStyle>
+            </Card.Section>
+            <Card.Section title="Items">
+              <TextStyle variation="subdued">301 units available</TextStyle>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+        <Layout.Section oneHalf>
+          <Card title="Nevada">
+            <Card.Section>
+              <TextStyle variation="subdued">301 units available</TextStyle>
+            </Card.Section>
+            <Card.Section title="Items">
+              <TextStyle variation="subdued">301 units available</TextStyle>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+      </Layout>
+      """
+    end
+  end
+
+  defmodule ThreeEqual do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Structure.Layout,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Three columns with equal width",
+      height: "270px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
+
+    alias SurfacePolaris.Structure.Card
+    alias SurfacePolaris.TitlesText.TextStyle
+
+    def render(assigns) do
+      ~F"""
+      <Layout>
+        <Layout.Section oneThird>
+          <Card title="Florida">
+            <Card.Section>
+              <TextStyle variation="subdued">455 units available</TextStyle>
+            </Card.Section>
+            <Card.Section title="Items">
+              <TextStyle variation="subdued">1931 units available</TextStyle>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+        <Layout.Section oneThird>
+          <Card title="Nevada">
+            <Card.Section>
+              <TextStyle variation="subdued">301 units available</TextStyle>
+            </Card.Section>
+            <Card.Section title="Items">
+              <TextStyle variation="subdued">1931 units available</TextStyle>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+        <Layout.Section oneThird>
+          <Card title="Minneapolis">
+            <Card.Section>
+              <TextStyle variation="subdued">1931 units available</TextStyle>
+            </Card.Section>
+            <Card.Section title="Items">
+              <TextStyle variation="subdued">1931 units available</TextStyle>
+            </Card.Section>
           </Card>
         </Layout.Section>
       </Layout>
