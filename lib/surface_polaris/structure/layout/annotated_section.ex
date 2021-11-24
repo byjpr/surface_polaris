57867b370f3e86c99dc55e35d54830340ddc647f
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2021-11-24 01:21:35 +0000

    Wrap with TextContainer

--- lib/surface_polaris/structure/layout/annotated_section.ex
+++ lib/surface_polaris/structure/layout/annotated_section.ex
@@ -8,15 +8,15 @@ defmodule SurfacePolaris.Structure.Layout.AnnotatedSection do
 
   def render(assigns) do
     ~F"""
-
-
     <div class={["Polaris-Layout__AnnotatedSection"]}>
       <div class={["Polaris-Layout__AnnotationWrapper"]}>
         <div class={["Polaris-Layout__Annotation"]}>
-          <SurfacePolaris.TitlesText.Heading element="h2">{@title}</SurfacePolaris.TitlesText.Heading>
-          <div class={["Polaris-Layout__AnnotationDescription"]}>
-            <p>{@description}</p>
-          </div>
+          <SurfacePolaris.TitlesText.TextContainer>
+            <SurfacePolaris.TitlesText.Heading element="h2">{@title}</SurfacePolaris.TitlesText.Heading>
+            <div class={["Polaris-Layout__AnnotationDescription"]}>
+              <p>{@description}</p>
+            </div>
+          </SurfacePolaris.TitlesText.TextContainer>
         </div>
         <div class={["Polaris-Layout__AnnotationContent"]}>
           <#slot />
