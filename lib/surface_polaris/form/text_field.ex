6dea301a03ce9c99995e4d14f64260ff9f5f0545
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-04-10 21:39:31 +0000

    Adds basic Text Field

---
+++ lib/surface_polaris/form/text_field.ex
@@ -0,0 +1,75 @@
+defmodule SurfacePolaris.Form.TextField do
+  @moduledoc """
+  A text field is an input field that merchants can type into.
+  It has a range of options and supports several text formats including numbers.
+  """
+
+  use Surface.Component
+
+  prop(type, :string,
+    values: [
+      "text",
+      "email",
+      "number",
+      "password",
+      "search",
+      "tel",
+      "url",
+      "date",
+      "datetime-local",
+      "month",
+      "time",
+      "week",
+      "currency"
+    ],
+    default: "text"
+  )
+
+  prop(alignment, :string,
+    values: [
+      "default",
+      "left",
+      "center",
+      "right"
+    ],
+    default: "default"
+  )
+
+  prop(input_mode, :string,
+    values: [
+      "none",
+      "text",
+      "decimal",
+      "numeric",
+      "tel",
+      "search",
+      "email",
+      "url"
+    ],
+    default: "text"
+  )
+
+  def align_class(input), do: "Polaris-TextField__Input--align#{input}"
+
+  def render(assigns) do
+    ~F"""
+    <div class={
+      "Polaris-TextField",
+      "#{align_class("left")}": @alignment == "left",
+      "#{align_class("center")}": @alignment == "center",
+      "#{align_class("right")}": @alignment == "right",
+    }>
+      <input
+        id="PolarisTextField2"
+        autocomplete="off"
+        class="Polaris-TextField__Input"
+        aria-labelledby="PolarisTextField2Label"
+        aria-invalid="false"
+        type={"#{@type}"}
+        inputmode={"#{@input_mode}"}
+        value="">
+      <div class="Polaris-TextField__Backdrop"></div>
+    </div>
+    """
+  end
+end
