ef940b0ee106c50f1307fe2e7148aef5eb0e38d4
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-04-10 21:24:47 +0000

    add formlayout

---
+++ lib/surface_polaris/form/form_layout.ex
@@ -0,0 +1,19 @@
+defmodule SurfacePolaris.Form.FormLayout do
+  @moduledoc """
+  Use form layout to arrange fields within a form using
+  standard spacing. By default it stacks fields vertically
+  but also supports horizontal groups of fields.
+  """
+
+  use Surface.Component
+
+  slot default
+
+  def render(assigns) do
+    ~F"""
+    <div class="Polaris-FormLayout">
+      <#slot />
+    </div>
+    """
+  end
+end
