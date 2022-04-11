eb1fd6337628bb52f9740bebe3ec74fc3001ff90
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-04-11 20:34:18 +0000

    Adds recase to handle data transforming

--- mix.exs
+++ mix.exs
@@ -68,7 +68,8 @@ defmodule SurfacePolaris.MixProject do
       {:ecto, "~> 3.4.2", only: [:test, :dev]},
       {:exsync, "~> 0.2", only: :dev},
       {:surface_formatter, "~> 0.7.5", only: :dev},
-      {:polaris_icon, path: "../polaris_icon"}
+      {:polaris_icon, path: "../polaris_icon"},
+      {:recase, "~> 0.5"}
     ]
   end
 
