0c12423772258cb6e61edc7a7d8fa1a1bad8cff8
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-04-11 11:53:33 +0000

    Throw if fieldID missing, don't render without message, adds docs

--- lib/surface_polaris/form/inline_error.ex
+++ lib/surface_polaris/form/inline_error.ex
@@ -10,14 +10,18 @@ defmodule SurfacePolaris.Form.InlineError do
   use Surface.Component
 
   @doc "Unique identifier of the invalid form field that the message describes"
-  prop(fieldID, :string)
+  prop(fieldID, :string, required: true)
 
-  @doc "Content briefly explaining how to resolve the invalid form field input"
+  @doc "Content briefly explaining how to resolve the invalid form field input. If missing or empty string the component will not render."
   prop(message, :string)
 
   def render(assigns) do
+    unless assigns[:fieldID] || Keyword.get(assigns.opts, :fieldID) do
+      raise ArgumentError, "<Form.InlineError /> requires a fieldID prop"
+    end
+
     ~F"""
-    <div id={@fieldID} class="Polaris-InlineError">
+    <div id={@fieldID} class="Polaris-InlineError" :if={@message && (@message != "")}>
       <div class="Polaris-InlineError__Icon">
         <span class="Polaris-Icon"><span class="Polaris-VisuallyHidden"></span><svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true"><path d="M10 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16zM9 9a1 1 0 0 0 2 0V7a1 1 0 1 0-2 0v2zm0 4a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"></path></svg></span>
       </div>
