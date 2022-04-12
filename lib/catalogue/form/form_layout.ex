5fa004a6717356aa7e4ffa97d89fdcb223836b46
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-04-12 22:35:44 +0000

    Add FormLayout Catalogue

---
+++ lib/catalogue/form/form_layout.ex
@@ -0,0 +1,112 @@
+defmodule SurfacePolaris.Catalogue.FormLayout do
+  @moduledoc """
+  """
+
+  defmodule Default do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Form.FormLayout,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Default behavior",
+      height: "140px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
+
+    alias SurfacePolaris.{Structure, Form}
+
+    @moduledoc """
+    Use to stack form fields vertically, which makes them easier to scan and complete.
+    """
+
+    def render(assigns) do
+      ~F"""
+      <FormLayout>
+        <FormLayout.Item>
+          <Form.TextField
+            label="Product weight"
+            type="number"
+          />
+        </FormLayout.Item>
+        <FormLayout.Item>
+          <Form.Select
+            label="Unit of measure"
+            placeholder="Select"
+          />
+        </FormLayout.Item>
+      </FormLayout>
+      """
+    end
+  end
+
+  defmodule CondensedGroup do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Form.FormLayout,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Condensed Group",
+      height: "140px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
+
+    alias SurfacePolaris.{Structure, Form}
+
+    @moduledoc """
+    For very short inputs, the width of the inputs may be reduced in order to fit more fields in the row.
+    """
+
+    def render(assigns) do
+      ~F"""
+      <FormLayout>
+        <FormLayout.Group condensed>
+          <FormLayout.Item>
+            <Form.TextField label="Length" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Width" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Height" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Unit" />
+          </FormLayout.Item>
+        </FormLayout.Group>
+      </FormLayout>
+      """
+    end
+  end
+
+  defmodule Group do
+    use Surface.Catalogue.Example,
+      subject: SurfacePolaris.Form.FormLayout,
+      catalogue: SurfacePolaris.Catalogue,
+      title: "Group",
+      height: "140px",
+      container: {:div, class: "temporary-workaround"},
+      direction: "vertical"
+
+    alias SurfacePolaris.{Structure, Form}
+
+    @moduledoc """
+    """
+
+    def render(assigns) do
+      ~F"""
+      <FormLayout>
+        <FormLayout.Group>
+          <FormLayout.Item>
+            <Form.TextField label="Length" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Width" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Height" />
+          </FormLayout.Item>
+          <FormLayout.Item>
+            <Form.TextField label="Unit" />
+          </FormLayout.Item>
+        </FormLayout.Group>
+      </FormLayout>
+      """
+    end
+  end
+end
