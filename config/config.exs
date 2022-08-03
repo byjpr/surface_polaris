74de1e84319a061f073975258dec96837824f390
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2022-08-03 17:00:07 +0000

    Add surface 0.7.0, add esbuild

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,24 @@ use Mix.Config
 
 config :phoenix, :json_library, Jason
 
+config :surface_catalogue,
+  title: "Shopify Polaris",
+  subtitle: "Polaris Surface UI Framework"
+
 config :surface,
   components: [
     {Surface.Components.Form.Field, [default_class: "field"]},
     {Surface.Components.Form.Label, [default_class: "label"]}
   ]
+
+config :esbuild,
+  version: "0.13.8",
+  default: [
+    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# When running `mix dev` inside `surface_catalogue`, there's no need to have the
+# assets in "/assets/catalogue" as they are the same we already have in `/assets`.
+config :surface_catalogue, :assets_path, "/assets"
--- dev.exs
+++ dev.exs
@@ -10,8 +10,14 @@ end
 
 # Start the catalogue server
 Surface.Catalogue.Server.start(
+  reloadable_compilers: [:phoenix, :elixir, :surface],
+  http: [port: 4001],
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+  ],
   live_reload: [
     patterns: [
+      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"lib/polaris/*/.*(ex)$"
     ]
   ]
